fc55de6159aec7282e8fe9004f6463feee89c3da
Jose Mateo Castano <jose.castano@CO-IT02063.local>
2019-12-02 13:26:36 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,33 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import mongoose from 'mongoose';
+
+import routes from './routes/index.js';
+
+const app = express();
+
+// DB Connection
+mongoose.connect('mongodb://localhost');
+
+// MiddleWare
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// catch 400
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(400).send(`Error: ${res.originUrl} not found`);
+    next();
+});
+
+// catch 500
+app.use((err, req, res, next) => {
+    console.log(err.stack)
+    res.status(500).send(`Error: ${err}`);
+    next();
+});
+
+// Register routes
+routes(app);
+
+export default app;
